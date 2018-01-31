171f24968c67671929fc2235596d68b3a242a62c
Olexiy <o.m.letushev@gmail.com>
2018-01-31 16:52:12 +0200

    Make time function safer

--- js/script.js
+++ js/script.js
@@ -1,10 +1,12 @@
-function startTime() {
-  var today = new Date();
-  var h = today.getHours().toString().padStart(2, '0');
-  var m = today.getMinutes().toString().padStart(2, '0');
-  var s = today.getSeconds().toString().padStart(2, '0');
-  document.getElementById('time').innerHTML = `${h}:${m}:${s}`;
-  window.setTimeout(() => startTime(), 500);
-}
-
-startTime();
+(function() {
+  var time = document.getElementById('time');
+  function startTime() {
+    var today = new Date();
+    var h = today.getHours().toString().padStart(2, '0');
+    var m = today.getMinutes().toString().padStart(2, '0');
+    var s = today.getSeconds().toString().padStart(2, '0');
+    time.innerHTML = `${h}:${m}:${s}`;
+  }
+  startTime();
+  window.setInterval(() => startTime(), 500);
+})()
